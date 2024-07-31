90dc042b9d172830d07346be8b4f83b4b828e784
matviiv andrij <matviivandrij13@gmail.com>
2024-07-31 14:47:13 +0300

    bugfix: took away the extra message

--- karg.API/Controllers/ContactController.cs
+++ karg.API/Controllers/ContactController.cs
@@ -21,25 +21,18 @@ namespace karg.API.Controllers
         /// Gets a list of contacts.
         /// </summary>
         /// <response code="200">Successful request. Returns a list of contacts.</response>
-        /// <response code="400">Invalid request parameters provided.</response>
         /// <response code="404">No contacts found.</response>
         /// <response code="500">An internal server error occurred while trying to get the list of contacts.</response>
         /// <returns>List of contacts.</returns>
         [HttpGet("getall")]
         [AllowAnonymous]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> GetAllContacts()
         {
             try
             {
-                if (!ModelState.IsValid)
-                {
-                    return BadRequest("Invalid request parameters provided.");
-                }
-
                 var contacts = await _contactService.GetContacts();
 
                 if (contacts.Count == 0)
